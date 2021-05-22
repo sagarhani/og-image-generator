63aa64350295a5087aefe88f35bd96114be4d8b5
sagarhani <sagarhani33@gmail.com>
2021-05-22 15:40:46 +0530

    👌🏻 Some more meta tags

--- pages/_app.js
+++ pages/_app.js
@@ -18,6 +18,10 @@ function MyApp({ Component, pageProps }) {
           property="og:image"
           content={`${process.env.NEXT_PUBLIC_APP_URL}/api/generate?title=Dynamic Open Graph Image Generator&author=Sagar Hani&avatar=https://avatars.githubusercontent.com/u/6933389&websiteUrl=https://sagarhani.in&theme=nightOwl`}
         />
+        <meta name="author" content="Sagar Hani" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@sagarhan_i" />
+        <meta name="twitter:creator" content="@sagarhan_i" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=PT+Mono&family=Poppins&family=Roboto&display=swap"
